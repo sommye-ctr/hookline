835e21d08c1a4e9b43a55b59010fc901c8a13f03
Somye <anyone.mahajan@gmail.com>
2025-06-25 21:00:28 +0530

    Registered the new models

--- workflows/admin.py
+++ workflows/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
-from workflows.models import Workspace, Workflow, Trigger, Action, ExecutionLog
+from workflows.models import Workspace, Workflow, Trigger, Action, ExecutionLog, WebhookEndpoint, InstalledPlugin
 
 admin.site.register(Workspace)
 admin.site.register(Workflow)
 admin.site.register(Trigger)
 admin.site.register(Action)
 admin.site.register(ExecutionLog)
+admin.site.register(WebhookEndpoint)
+admin.site.register(InstalledPlugin)
